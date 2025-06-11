11baad197f60dacfd0d6f1e8a75826825a12d000
Daniel <Danieldelreal17@gmail.com>
2025-06-10 21:10:46 -0400

    primera parte

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-echo "Print('Hola, Mundo')"
+from vehiculo import Vehiculo
+from basedatos import BaseDatos
+
+
+
+
+elif opcion == "3":
+     id_v = int(input("ID VEHICULO: "))
+     v = bd.buscar_vehiculo(id_v)
+    if v:
+     print(f"ID: VEHICULO")
